467e9751fbd2f4590b7877010adcddead531b743
Emil Lemming <Emil Lemming>
2016-12-05 13:01:28 +0100

    theater er rettet

--- Theater.java
+++ Theater.java
@@ -1,6 +1,13 @@
 public class Theater
 {
-    int row;
-    int seat;
-    Reservation [][] theater;
+    private int row;
+    private int seat;
+    private Reservation[][] theater;
+    
+    public Theater(int row, int seat)
+    {
+        this.row = row;
+        this.seat = seat;
+        theater = new Reservation[row][seat];
+    }    
 }
