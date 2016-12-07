2d093b67f0680ff66fbf268c4dabed50dda753ef
Emil Lemming <Emil Lemming>
2016-12-07 15:07:42 +0100

    added allShows method

--- MySystem.java
+++ MySystem.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+
 /**
  * The Great and Powerful Trix- er, System! Uses the Theater/Order/Reservation/Seat classes in a way
  * that fits with the theaters and showings at EBMCinema, drawing from MyDBSystem, and feeding View.
@@ -28,6 +30,28 @@ public class MySystem
     public void createOrder(String name, int phone)
     {
         order = new Order(name, phone);
+    }
+
+    public String[][] getAllShows()
+    {
+        ResultSet rs = mydb.getAllShows();
+        String[][] allShows = null;
+        try{
+            rs.last();
+            allShows = new String[rs.getRow()][5];
+            rs.beforeFirst(); 
+            for(int i=0; rs.next(); i++)
+            {
+                allShows[i][0]= rs.getString("ShowID");
+                allShows[i][1]= rs.getString("FIlm");
+                allShows[i][2]= rs.getString("TheaterID");
+                allShows[i][3]= rs.getString("Dato");
+                allShows[i][4]= rs.getString("Tid");
+            }
+        }
+        catch(Exception e){}
+
+        return allShows;
     }
 
     public void addReservation(Reservation reservation)
